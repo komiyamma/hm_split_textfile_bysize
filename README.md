89c41b84063672e210659d97b0f05592f73c522f
komiyamma <komiyama0629@gmail.com>
2024-05-20 10:44:41 +0900

    komitto

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# hm_split_textfile_bysize
+# HmSplitTextFileBySize
 秀丸で今開いているファイルを、指定のキロバイト数で分割する
+
+- インストール
+  
+  「HmSplitTextFileBySize.mac」と「SplitTextFileBySize.exe」を一緒のフォルダーに入れる
+
+- 使い方
+    分割したいテキストファイルを秀丸で開く
+    HmSplitTextFileBySize.mac を 実行する。
+    ⇒ 自動で 「ファイル名_001.txt」「ファイル名_002.txt」... といったように連番で分割される。
---
+++ src/SplitTextFileBySize/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace SplitTextFileBySize;
+
+internal class Program
+{
+
+    static string getDivitionFileName(string filePath, int num, int numberWidth)
+    {
+        var strNumber = num.ToString("D" + numberWidth);
+
+        // ファイル名を拡張子とそれ以外に分ける
+        string fileName = Path.GetFileNameWithoutExtension(filePath); // 拡張子を除いたファイル名を取得
+        string fileExtension = Path.GetExtension(filePath); // 拡張子を取得
+
+        string newFileName = fileName + "_" + strNumber + fileExtension;
+        return newFileName;
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            // argの1番目にファイル名
+            string filePath = args[0];
+
+            // argの2番目にカットしたい長さ
+            int targetCurLength = int.Parse(args[1]);
+
+            // argの3番目にエンコード
+            int targetEncodingCodePage = int.Parse(args[2]);
+
+            var encode = System.Text.Encoding.GetEncoding(targetEncodingCodePage);
+            // ファイルを読み込む
+            string allText = System.IO.File.ReadAllText(filePath, encode);
+
+            // 文字列をバイト配列に変換
+
+            List<string> fileTextList = new List<string>();
+            while (true)
+            {
+                int cutLength = targetCurLength;
+
+                if (cutLength > allText.Length)
+                {
+                    cutLength = allText.Length;
+                }
+                string headText = allText.Substring(0, cutLength); // 高速化するためにヘッダー
+                byte[] headerArray = System.Text.Encoding.GetEncoding(targetEncodingCodePage).GetBytes(headText);
+
+                byte[] cutArray = new byte[cutLength];
+                Array.Copy(headerArray, 0, cutArray, 0, cutLength);
+
+                try
+                {
+                    // カット後の配列を文字列に変換
+                    string cutStr = System.Text.Encoding.GetEncoding(targetEncodingCodePage).GetString(cutArray);
+
+                    if (headerArray.Length < targetCurLength)
+                    {
+                        fileTextList.Add(cutStr);
+                        break;
+                    }
+
+                    // curStrから1文字カット
+                    cutStr = cutStr.Substring(0, cutStr.Length - 1);
+                    // 末尾が\rなら削除
+                    if (cutStr.EndsWith("\r"))
+                    {
+                        cutStr = cutStr.Substring(0, cutStr.Length - 1);
+                    }
+                    fileTextList.Add(cutStr);
+
+                    // strからcutStrを先頭から削除
+                    allText = allText.Substring(cutStr.Length);
+
+                    if (allText == "")
+                    {
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            /*
+            for (int i = 0; i < fileTextList.Count; i++)
+            {
+                var newFilePath = getDivitionFileName(filePath, i, numberWidth);
+                // list.Countの数値の長さを0埋めする
+                Console.WriteLine(newFilePath);
+                // ファイルに書き込む
+                System.IO.File.WriteAllText(newFilePath, fileTextList[i], encode);
+
+            }
+            */
+
+            // ファイル作成は重いので、並列処理してしまう。
+            var count = fileTextList.Count;
+            var numberWidth = count.ToString().Length;
+            Parallel.For(0, fileTextList.Count, i =>
+            {
+                var newFilePath = getDivitionFileName(filePath, i+1, numberWidth);
+
+                // ファイルに書き込む
+                System.IO.File.WriteAllText(newFilePath, fileTextList[i], encode);
+            });
+
+            Console.WriteLine(count + "個のファイルに分割しました。");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
