55bc746cf2e84aa96ff24b97ce2e53d28b10b631
komiyamma <komiyama0629@gmail.com>
2024-05-20 13:28:06 +0900

    コメント追加

--- src/SplitTextFileBySize/Program.cs
+++ src/SplitTextFileBySize/Program.cs
@@ -50,6 +50,10 @@ internal class Program
             List<string> fileTextList = new List<string>();
             while (true)
             {
+                // まず、minLengthまわりの処理は不要なのであるが、「allText」をすべてバイト配列にすると、長大だと非常に不可が大きくなるため、
+                // 調査の対象を短くしている。
+                // 「最大でもallText」の長さ、「最低で」targetCurLengthの長さがあれば良い。
+                // なぜなら、「文字列」のバイト配列の長さは、かならず「文字列」の長さ(改行等含む)以上だからである。
                 int cutLength = targetCurLength;
                 int minLength = cutLength;
                 if (minLength > allText.Length)
@@ -64,23 +68,27 @@ internal class Program
                     cutLength = headerArray.Count();
                 }
 
+                // 調査範囲として、短くしたバイト配列を確保
                 byte[] cutArray = new byte[cutLength];
                 Array.Copy(headerArray, 0, cutArray, 0, cutLength);
 
                 try
                 {
-                    // カット後の配列を文字列に変換
+                    // 調査範囲のものをバイト範囲から再び文字列ｊに戻す
                     string cutStr = System.Text.Encoding.GetEncoding(targetEncodingCodePage).GetString(cutArray);
 
+                    // その長さが、指定されているバッファーよりも短いのであれば、最早調査する必要はない。
+                    // まるまるその文字列は、１つのファイルに対応する形で書き込む対象となる。
                     if (headerArray.Length < targetCurLength)
                     {
                         fileTextList.Add(cutStr);
                         break;
                     }
 
-                    // curStrから1文字カット
+                    // 一番最後の「1文字」はバイトが中途半端で壊れている可能性がきわめて高いので1文字カット（次のループで処理される）
                     cutStr = cutStr.Substring(0, cutStr.Length - 1);
-                    // 末尾が\rなら削除
+
+                    // 末尾が\rならカット(次のループで処理される)
                     if (cutStr.EndsWith("\r"))
                     {
                         cutStr = cutStr.Substring(0, cutStr.Length - 1);
@@ -89,17 +97,21 @@ internal class Program
                     // 改行を考慮するモード
                     if (targetLineMode == 1)
                     {
+                        // 改行あるならば、そこまでを範囲とする。(残りは次のループで処理される）
                         int lastNewLineIndex = cutStr.LastIndexOf('\n');
                         if (lastNewLineIndex != -1)
                         {
                             cutStr = cutStr.Substring(0, lastNewLineIndex + 1);
                         }
                     }
+
+                    // ここまで処理した文字列を１つのファイルに対応する文字列として登録
                     fileTextList.Add(cutStr);
 
-                    // strからcutStrを先頭から削除
+                    // 全体の文字列の先頭から先ほど使用した文字列分を削除して、次の処理へ
                     allText = allText.Substring(cutStr.Length);
 
+                    // 削除したら空っぽになったのなら、次のループに入るまでもなくここで終了
                     if (allText == "")
                     {
                         break;
